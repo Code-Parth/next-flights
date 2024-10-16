3636114eb6c3e7328d97c757b118e6fecbeabce9
Delba de Oliveira <delbabrown@gmail.com>
2024-10-16 13:06:36 +0100

    Use next@canary, use pnpm, update packages, cleanup

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.mapbox.com",
+      },
+    ],
+  },
+  experimental: {
+    dynamicIO: true,
+    // serverComponentsHmrCache: false,
+  },
+}
+
+export default nextConfig
