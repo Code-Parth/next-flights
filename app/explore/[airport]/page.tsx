c94159c8e2df1a8f729a08df3b43d739797cf8d0
Delba de Oliveira <delbabrown@gmail.com>
2024-10-17 15:35:58 +0100

    Clean up

--- app/explore/[airport]/page.tsx
+++ app/explore/[airport]/page.tsx
@@ -2,9 +2,6 @@ import { getFlights } from "@/app/ui/get-flights"
 import { Map } from "@/app/ui/map"
 import Link from "next/link"
 
-type Params = Promise<{
-  airport: string // e.g. SFO
-}>
 export default async function Page({ params }: { params: Params }) {
   const flights = await getFlights(params)
 
@@ -19,3 +16,7 @@ export default async function Page({ params }: { params: Params }) {
     </div>
   )
 }
+
+type Params = Promise<{
+  airport: string // e.g. SFO
+}>
