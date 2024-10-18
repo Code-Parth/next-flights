a311065d503f2bf108856d77263907f8c147ad36
Delba de Oliveira <delbabrown@gmail.com>
2024-10-18 06:10:27 +0100

    add flight tracks data

--- app/explore/[airport]/page.tsx
+++ app/explore/[airport]/page.tsx
@@ -1,9 +1,12 @@
+import { getFlightTracks } from '@/app/ui/get-flight-tracks';
 import { getFlights } from '@/app/ui/get-flights';
 import { Map } from '@/app/ui/map-2';
 
-
 export default async function Page({ params }: { params: Params }) {
   const flights = await getFlights(params);
+
+  // show when flight is selected:
+  const flightTracks = await getFlightTracks(flights[0].fr24_id);
 
   return (
     <div className="w-screen h-screen">
