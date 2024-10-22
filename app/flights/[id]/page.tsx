5c94c21755960349e7e8965cf7f96f88911924fb
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-10-22 11:38:50 -0700

    update next

--- app/flights/[id]/page.tsx
+++ app/flights/[id]/page.tsx
@@ -1,12 +1,13 @@
 import { FlightActivity } from '@/app/ui/flight-activity';
 import { PlaneDetails } from '@/app/ui/plane-details';
+import { Suspense } from 'react';
 
 export default function Page({ params }: { params: Params }) {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <PlaneDetails params={params} />
       <FlightActivity params={params} />
-    </>
+    </Suspense>
   );
 }
 
